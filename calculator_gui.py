221ba693c4ed3f052cc865bd1dc78286b7faa421
Marchharee <marchhare070314@gmail.com>
2025-02-17 09:53:18 +0000

    Update calculator_gui.py
    create the input fied and buttons for the calculator

--- calculator_gui.py
+++ calculator_gui.py
@@ -25,3 +25,29 @@ class Calculator:
         self.input_text = tk.StringVar()
         #create the input field and buttons
         self.create_widgets()
+        
+   def create_widgets(self):
+        """create the input field and buttons for the calculator"""
+        #create the entry field 
+        entry_frame = tk.Frame(self.root)
+        #allow the frame to fill width and height
+        entry_frame.pack(expand=True, fill="both")
+        #create a input box and set the font size set the border width
+        entry = tk.Entry(entry_frame, font=("Arial", 20), textvariable=self.input_text, justify="right", bd=10, relief=tk.GROOVE)
+        entry.pack(expand=True, fill="both")
+        #create the button box
+        button_frame = tk.Frame(self.root)
+        button_frame.pack(expand=True, fill="both")
+        #create digital buttons
+        buttons = [
+            ("7", "8", "9", "/"),
+            ("4", "5", "6", "*"),
+            ("1", "2", "3", "-"),
+            ("0", ".", "=", "+"),
+        ]
+        #create calculate buttons
+        sci_buttons = [
+            ("sin", "cos", "tan", "√"),
+            ("asin", "acos", "atan", "x²"),
+            ("C", "(", ")", "Del"),
+        ]
