ff5ad7462c0947cd8f171c478a082c74532b88b6
Thamer A Sh K Hasoon <thameradel96@gmail.com>
2025-02-21 16:00:54 +0000

    Add Support for sin, cos, and tan Functions
    Add support for trigonometric functions: sin, cos, tan.

--- calculator.py
+++ calculator.py
@@ -2,3 +2,14 @@ import math
 def calculate(expression):
     
     expression = expression.replace('π', str(math.pi))
+    
+    # add scientific functions
+    if 'sin' in expression:
+        value = float(expression.replace('sin', '').strip('() '))
+        return round(math.sin(math.radians(value)), 4)
+    elif 'cos' in expression:
+        value = float(expression.replace('cos', '').strip('() '))
+        return round(math.cos(math.radians(value)), 4)
+    elif 'tan' in expression:
+        value = float(expression.replace('tan', '').strip('() '))
+        return round(math.tan(math.radians(value)), 4)
