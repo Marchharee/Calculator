948e0df7c9db8a3b5c109015e2700374620d2d30
Thamer A Sh K Hasoon <thameradel96@gmail.com>
2025-02-21 16:04:22 +0000

    Add Support for arcsin, arccos, and arctan Functions
    Add support for inverse trigonometric functions: arcsin, arccos, arctan.

--- calculator.py
+++ calculator.py
@@ -13,3 +13,12 @@ def calculate(expression):
     elif 'tan' in expression:
         value = float(expression.replace('tan', '').strip('() '))
         return round(math.tan(math.radians(value)), 4)
+    elif 'arcsin' in expression:
+        value = float(expression.replace('arcsin', '').strip('() '))
+        return round(math.degrees(math.asin(value)), 4)
+    elif 'arccos' in expression:
+        value = float(expression.replace('arccos', '').strip('() '))
+        return round(math.degrees(math.acos(value)), 4)
+    elif 'arctan' in expression:
+        value = float(expression.replace('arctan', '').strip('() '))
+        return round(math.degrees(math.atan(value)), 4)
